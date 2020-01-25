8faa5fa0b58145e208a7e7a62331c6ab713e3aee
Darya Kulikova <letsgoriot1@gmail.com>
2020-01-26 00:14:49 +0300

    Задача на сравнение строк с комментариями.

--- src/main/java/Exercise/StringCompare.java
+++ src/main/java/Exercise/StringCompare.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class StringCompare {
 
-    public boolean compareStrings(String stringOne, String stringTwo) {
-        int unmapedLettersCount = 0;
+    public boolean compareStrings(String stringOne, String stringTwo) { //создаем метод сравнения строк
+        int unmapedLettersCount = 0; //объявляем переменную переменную счетчик лишних символов
 
-        List<Character> myList = splitStringToList(stringOne);
+        List<Character> myList = splitStringToList(stringOne); //разбиваем строки и помещаем посимвольно в листы
         List<Character> myList2 = splitStringToList(stringTwo);
 
         if (stringOne.length() >= stringTwo.length() + 2 || stringTwo.length() >= stringOne.length() + 2) {
-            return false;
+            return false; //создаем условие, при котором длина строк не может отличатся больше чем на один символ.
         }
 
-        for (int k = 0; k < myList.size(); k++) {
+        for (int k = 0; k < myList.size(); k++) { //сравнивам строки посимвольно, чтобы найти несовпадающие символы
             if (!myList2.contains(myList.get(k))) {
                 unmapedLettersCount += 1;
             }
@@ -28,7 +28,7 @@ public class StringCompare {
         }
     }
 
-    private List<Character> splitStringToList(String s) {
+    private List<Character> splitStringToList(String s) { //метод разбивает строки на символы и добавляющий их в лист
         ArrayList<Character> list = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
             list.add(s.charAt(i));
